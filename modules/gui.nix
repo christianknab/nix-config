2923b679d4757e15139b80d13ca59be78e162fd5
Christian Knab <christiantknab@gmail.com>
2026-04-27 16:46:19 -0700

    brave + rectangle + spotify

--- modules/gui.nix
+++ modules/gui.nix
@@ -4,6 +4,15 @@
       obsidian
       signal-desktop
       zoom-us
+      brave
+      rectangle
+      spotify
     ];
+
+    # track the Rectangle config. on launch it will load the config file at the following path
+    # must be exported and imported in Rectangle gui
+    # https://github.com/rxhanson/rectangle#import--export-json-config
+    home.file."Library/Application Support/Rectangle/RectangleConfig.json".source =
+      ./data/RectangleConfig.json;
   };
 }
