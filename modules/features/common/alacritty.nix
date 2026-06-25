d26b9445a0e5f0ece4bd688091f3537ce6b13113
Christian Knab <christiantknab@gmail.com>
2026-06-24 18:15:59 -0700

    font info

--- modules/features/common/alacritty.nix
+++ modules/features/common/alacritty.nix
@@ -1,29 +1,35 @@
 { ... }: {
-  flake.sharedModules.alacritty = { config, ... }: {
+  flake.sharedModules.alacritty = { pkgs, config, ... }: {
     home-manager.users.${config.mainUser} = {
       programs.alacritty = {
         enable = true;
         settings = {
+		  # `defaults write -g AppleFontSmoothing -int 0` to stop mac bolding
+		  # ref: https://github.com/alacritty/alacritty/issues/4616
           font = {
             normal = {
               family = "JetBrainsMono Nerd Font";
               style = "Regular";
             };
-            bold = {
-              family = "JetBrainsMono Nerd Font";
-              style = "Bold";
-            };
-            italic = {
-              family = "JetBrainsMono Nerd Font";
-              style = "Italic";
-            };
-            bold_italic = {
-              family = "JetBrainsMono Nerd Font";
-              style = "Bold Italic";
-            };
+            # bold = {
+            #   family = "JetBrainsMono Nerd Font";
+            #   style = "Bold";
+            # };
+            # italic = {
+            #   family = "JetBrainsMono Nerd Font";
+            #   style = "Italic";
+            # };
+            # bold_italic = {
+            #   family = "JetBrainsMono Nerd Font";
+            #   style = "Bold Italic";
+            # };
             size = 13;
           };
+terminal.shell = {
+          program = "${pkgs.zsh}/bin/zsh";
         };
+        };
+        
       };
     };
   };
