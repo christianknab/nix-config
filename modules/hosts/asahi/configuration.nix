8f4adfef74036b2c00cae410a75db7c4d20d53b6
Christian Knab <christiantknab@gmail.com>
2026-06-30 21:46:17 -0700

    asahi update

--- modules/hosts/asahi/configuration.nix
+++ modules/hosts/asahi/configuration.nix
@@ -6,9 +6,41 @@
       inputs.self.sharedModules.zsh
       inputs.self.sharedModules.tmux
       inputs.self.sharedModules.direnv
+      ./hardware-configuration.nix
       inputs.self.modules.nixos.base
+      ./apple-silicon-support
     ];
 
-    networking.hostName = "asahi";
+    networking.hostName = "christianknab";
+    boot.loader.systemd-boot.enable = true;
+    boot.loader.efi.canTouchEfiVariables = false;
+    boot.extraModprobeConfig = ''
+	options hid_apple iso_layout=0
+    '';
+    hardware.asahi.peripheralFirmwareDirectory = ./firmware;
+    time.timeZone = "America/Los_Angeles";
+services.libinput.enable = true;
+services.xserver.enable = true;
+services.xserver.desktopManager.xfce.enable = true;
+    
+   home-manager.users.christianknab = { pkgs, ... }: {
+        home.stateVersion = "26.11";
+        home.packages = [
+          pkgs.opencode
+          pkgs.sl
+          pkgs.neovim
+        ];
+        programs.zoxide.enable = true;
+        programs.eza.enable = true;
+        programs.fd.enable = true;
+        programs.bat = {
+          enable = true;
+          config = {
+            paging = "never";
+            style = "plain";
+          };
+        };
+      };
+    };
   };
 }
