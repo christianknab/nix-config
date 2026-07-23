aa045e3479e6c68b512d5e922d36d4ca02502e26
Christian Knab <christiantknab@gmail.com>
2026-07-22 22:49:05 -0700

    brightness + volume + mic keys

--- modules/features/desktop/default.nix
+++ modules/features/desktop/default.nix
@@ -52,6 +52,12 @@
     ];
 
     hardware.bluetooth.enable = true;
+    services.pipewire = {
+      enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      pulse.enable = true;
+    };
     services.power-profiles-daemon.enable = true;
     services.upower.enable = true;
 
